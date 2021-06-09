ebd98efbbb0cf37a06ee1c94bf2849085957bd08
JoniHan <joni.hanninen@hotmail.com>
2021-06-09 22:30:16 +0300

    swapped own app components to replace the stock one

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MainView from './views/main-view';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MainView />
   </React.StrictMode>,
   document.getElementById('root')
 );
