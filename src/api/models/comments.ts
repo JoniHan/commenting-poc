288f569373a3ff5159705a63658583dfe9acfc4c
JoniHan <joni.hanninen@hotmail.com>
2021-06-09 22:28:52 +0300

    added api models

---
+++ src/api/models/comments.ts
@@ -0,0 +1,5 @@
+import { IGeneralObjectProps } from "./general";
+
+export interface ICommentProps extends IGeneralObjectProps {
+    caption: string;
+};
---
+++ src/api/models/items.ts
@@ -0,0 +1,6 @@
+import { IGeneralObjectProps } from "./general";
+
+export interface IItemProps extends IGeneralObjectProps {
+    title: string;
+    caption: string;
+};
